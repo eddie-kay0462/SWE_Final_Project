1c9df3d09a91ed67e659636ce8bd73ab406c196f
Edward Ofosu Mensah <edward.mensah@ashesi.edu.gh>
2025-03-15 23:39:15 +0000

    Adjust to run file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add your webpack configurations here if needed
+    return config;
+  },
+  images: {
+    domains: ['images.unsplash.com'], // Add any external image domains you need
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
