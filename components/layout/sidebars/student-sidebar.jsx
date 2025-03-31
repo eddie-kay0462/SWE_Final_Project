5e1c1ec5f59a7d2d83edfaf3ad81f4f36d5ffde1
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-03-31 14:19:10 +0000

    updated sidebar navigation

--- components/layout/sidebars/student-sidebar.jsx
+++ components/layout/sidebars/student-sidebar.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { LayoutDashboard, FileText, Calendar, BarChart3, Bell, Settings, UserPen, MessageCircle, FileUser, CalendarCheck } from "lucide-react"
+import { LayoutDashboard, FileText, BarChart3, Bell, Settings, UserPen, MessageCircle, FileUser, CalendarClock } from "lucide-react"
 import Sidebar from "../sidebar"
 import Dashboard from "@/app/dashboard/page"
 
@@ -9,10 +9,8 @@ const studentNavItems = [
   { name: "Profile", href: "/dashboard/student/profile", icon: UserPen },
   { name: "Resume", href: "/dashboard/student/resume", icon: FileUser },
   { name: "Internship Requests", href: "/dashboard/student/internship-request", icon: FileText },
-  { name: "1-on-1 Sessions", href: "/dashboard/student/sessions", icon: CalendarCheck },
-  // { name: "Career Sessions", href: "/dashboard/sessions", icon: Calendar },
+  { name: "1-on-1 Sessions", href: "/dashboard/student/one-on-one", icon: CalendarClock },
   { name: "Attendance History", href: "/dashboard/student/attendance-history", icon: BarChart3 },
-  // { name: "Career Resources", href: "/dashboard/resources", icon: FileText },
   { name: "Notifications", href: "/dashboard/student/notifications", icon: Bell },
   // { name: "Chat", href: "/dashboard/chat", icon: MessageCircle },
   // { name: "Settings", href: "/dashboard/settings", icon: Settings },
@@ -32,9 +30,7 @@ export default StudentSidebar
 // Resumes -- View, Update and Delete resumes
 // Internship Request Letters -- Submit internship request letter [must meet specific requirements]
 // 1-on-1 Sessions -- Book 1-on-1 Sessions with career advisor
-////////////// Career Sessions -- View recents career sessions , view past and upcoming as well -- Don't need { Shows on event page }
 // Attendance History -- Track session attendance {Sessions attended and missed}
-////////////// Career Resources -- View career resources {CV guides, Cover Letter Templates, Career Tips, other advice}
 // Notifications -- Notified of Current Career Session {30 mins notification}
 ////////////// Chat -- Send and recieve messages from admin { Future Implementation }
 ////////////// Settings
