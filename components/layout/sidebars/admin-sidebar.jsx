58462d197294392c5fed7222cc13d3a7e74eeed6
Ronelle Cudjoe <ronnycudjoe9@gmail.com>
2025-04-07 16:21:38 +0000

    updated admin sidebar

--- components/layout/sidebars/admin-sidebar.jsx
+++ components/layout/sidebars/admin-sidebar.jsx
@@ -6,7 +6,7 @@ import Sidebar from "../sidebar"
 const adminNavItems = [
   { name: "Dashboard", href: "/dashboard/admin", icon: LayoutDashboard },
   { name: "Profile", href: "/dashboard/admin/profile", icon: UserPen },
-  { name: "Internship Requests", href: "/dashboard/admin/internship-requests", icon: FileText },
+  { name: "Internship Requests", href: "/dashboard/admin/internship-request", icon: FileText },
   { name: "1-on-1 Sessions", href: "/dashboard/admin/sessions", icon: CalendarCheck },
   { name: "Events", href: "/dashboard/admin/events", icon: Calendar },
   // { name: "Attendance Reports", href: "/dashboard/admin/attendance", icon: BarChart3 },
